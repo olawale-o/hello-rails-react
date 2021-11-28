786c6167fd244bd87b0972c908e39d9ea0162314
olawale-o <omoogunolawale@gmail.com>
2021-11-28 18:23:22 +0100

    Use react-router in App component

--- app/javascript/components/App.js
+++ app/javascript/components/App.js
@@ -1,10 +1,16 @@
 import React from "react"
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import PropTypes from "prop-types"
+import Greeting from "./Greeting";
+
 class App extends React.Component {
   render () {
     return (
-      <React.Fragment>
-      </React.Fragment>
+      <Router>
+        <Routes>
+          <Route path='/' element={<Greeting />} />
+        </Routes>
+      </Router>
     );
   }
 }
