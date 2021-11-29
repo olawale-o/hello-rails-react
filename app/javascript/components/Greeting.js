ee59eced2138649832a98ea8a84ceb995cb66748
olawale-o <omoogunolawale@gmail.com>
2021-11-29 06:19:03 +0100

    Show loading indicator

--- app/javascript/components/Greeting.js
+++ app/javascript/components/Greeting.js
@@ -3,10 +3,10 @@ import { useSelector } from 'react-redux';
 import Loading from './Loading';
 
 const Greeting = () => {
-  const { message, isLoading } = useSelector(state => state.greetings);
+  const { greeting, isLoading } = useSelector(state => state.greetings);
   return (
     <div className="greetings">
-      { isLoading ? <Loading /> : <h1>Greeting: {message}</h1>}
+      { isLoading ? <Loading /> : <h1>Greeting: {greeting}</h1>}
     </div>
   );
 };
--- app/javascript/redux/greetings/index.js
+++ app/javascript/redux/greetings/index.js
@@ -1,25 +1,41 @@
 import { getRandomMessage } from '../api';
 const GET_RANDOM_GREETING = 'greetings/random';
+const SHOW_LOADING = 'greetings/showLoading';
+const HIDE_LOADING = 'greetings/hideLoading';
 
 export const getRandomGreeting = (payload) => ({
   type: GET_RANDOM_GREETING,
   payload,
 });
 
+export const showLoading = () => ({
+  type: SHOW_LOADING,
+});
+
+export const hideLoading = () => ({
+  type: HIDE_LOADING,
+});
+
 export const randomGreeting = () => async (dispatch) => {
+  dispatch(showLoading());
   const data = await getRandomMessage();
   dispatch(getRandomGreeting(data));
+  dispatch(hideLoading());
 };
 
 const initialState = {
-  message: null,
+  greeting: null,
   isLoading: true,
 };
 
 const reducer = (state = initialState, action) => {
   switch (action.type) {
     case GET_RANDOM_GREETING:
-      return { ...state, message: action.payload.greeting, isLoading: false };
+      return action.payload;
+    case SHOW_LOADING:
+      return { ...state, isLoading: true };
+    case HIDE_LOADING:
+      return { ...state, isLoading: false };
     default:
       return state;
   }
